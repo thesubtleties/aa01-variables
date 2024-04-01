e1ce944d650743621f9621ba7262ddbca6dc756e
Steven <stevenglab@gmail.com>
2024-04-01 12:45:44 -0700

    Completed

---
+++ aa01-intro-to-expressions/problems/02-code-comments.js
@@ -0,0 +1,29 @@
+/*
+Use your newfound knowledge of comments to execute the prompts in this problem
+file.
+*/
+
+/* 1. This will print 'Welcome to App Academy!'  */
+
+console.log('Welcome to App Academy!');
+
+/* Print more messages to the terminal using console.log */
+console.log('One more console.log message here.');
+console.log('And one more for good measure.');
+
+// 2. This is a single line comment.
+// console.log('This is will not be printed to the terminal')
+// Try adding your own single line comments:
+// Your code here 
+// This is a single line comment by Steven.
+
+/* 3.
+This is a multi-line comment.
+Try adding your own multi-line comments:
+*/
+
+// Your code here 
+
+/* This is a 
+Multi-Line
+Comment */
